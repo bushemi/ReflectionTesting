0ee2a3d48c5e8688bea4d47a151dcf3da76a4c13
bushemi <80959051487>
2017-07-30 14:17:55 +0300

    Fixed bug NullPointerException if no argument in json

---
+++ src/main/java/dto/UserProfile.java
@@ -0,0 +1,65 @@
+package dto;
+
+import annotations.JsonValue;
+
+public class UserProfile {
+    private long id;
+    @JsonValue(name = "nick_name")
+    private String login;
+    private String password;
+    @JsonValue(name = "mail")
+    private String email;
+
+    public UserProfile(long id, String login, String password, String email) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserProfile() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UserProfile{");
+        sb.append("id=").append(id);
+        sb.append(", login='").append(login).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
